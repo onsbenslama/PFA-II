9f176800f85a425f13e1ae82dc0f4f077e452139
Ons Ben Slema <onsbenslema@gmail.com>
2021-05-05 02:50:59 +0100

    First commit

---
+++ src/components/main/Main.js
@@ -0,0 +1,97 @@
+import "./Main.css";
+import Chart from "../charts/Chart";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="main__container">
+        {/* <!-- MAIN TITLE STARTS HERE --> */}
+
+        <div className="main__title">
+          <div className="main__greeting">
+            <h1>Bienvenue</h1>
+            <p>Bienvenue dans votre dashboard</p>
+          </div>
+        </div>
+
+        {/* <!-- MAIN TITLE ENDS HERE --> */}
+
+        {/* <!-- MAIN CARDS STARTS HERE --> */}
+        <div className="main__cards">
+          <div className="card">
+            <i
+              className="fa fa-user-o fa-2x text-lightblue"
+              aria-hidden="true"
+            ></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Nombre des chambres</p>
+              <span className="font-bold text-title">578</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <i className="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Nombre des Patiens</p>
+              <span className="font-bold text-title">2467</span>
+            </div>
+          </div>
+
+          
+
+        
+        </div>
+        {/* <!-- MAIN CARDS ENDS HERE --> */}
+
+        {/* <!-- CHARTS STARTS HERE --> */}
+        <div className="charts">
+          <div className="charts__left">
+            <div className="charts__left__title">
+              <div>
+                <h1>Stock</h1>
+                <p>Tunisie,tunis</p>
+              </div>
+              <i className="fa fa-usd" aria-hidden="true"></i>
+            </div>
+            <Chart />
+          </div>
+
+          <div className="charts__right">
+            <div className="charts__right__title">
+              <div>
+                <h1> Stock disponible</h1>
+                <p>Tunisie,tunis</p>
+              </div>
+              <i className="fa fa-usd" aria-hidden="true"></i>
+            </div>
+
+            <div className="charts__right__cards">
+              <div className="card1">
+                <h1>stock 1</h1>
+                <p>Nombre</p>
+              </div>
+
+              <div className="card2">
+                <h1>Stock2</h1>
+                <p>nombre</p>
+              </div>
+
+              <div className="card3">
+                <h1>Stock3</h1>
+                <p>nombre</p>
+              </div>
+
+              <div className="card4">
+                <h1>stock3</h1>
+                <p>nombre</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <!-- CHARTS ENDS HERE --> */}
+      </div>
+    </main>
+  );
+};
+
+export default Main;
